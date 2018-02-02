61ae06a6dca338d7be1b574375635426317ded50
Mattheo <mattheoioannou@gmail.com>
2018-02-02 00:18:21 -0800

    Added camera view to main activity with a button at the bottom

--- app/src/main/java/solarsitingucsc/smartsolarsiting/MainActivity.java
+++ app/src/main/java/solarsitingucsc/smartsolarsiting/MainActivity.java
@@ -1,13 +1,63 @@
 package solarsitingucsc.smartsolarsiting;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Camera mCamera;
+    private CameraPreview mPreview;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        //Remove notification bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (checkCameraHardware(this)) {
+            mCamera = getCameraInstance();
+            mCamera.setDisplayOrientation(90);
+            mPreview = new CameraPreview(this, mCamera);
+            FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+            preview.addView(mPreview);
+        }
+    }
+
+    /** Check if this device has a camera */
+    private boolean checkCameraHardware(Context context) {
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+            // this device has a camera
+            return true;
+        } else {
+            // no camera on this device
+            return false;
+        }
+    }
+
+    /** A safe way to get an instance of the Camera object. */
+    public Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            // attempt to get a Camera instance
+            c = Camera.open();
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+
+        // returns null if camera is unavailable
+        return c;
     }
 }

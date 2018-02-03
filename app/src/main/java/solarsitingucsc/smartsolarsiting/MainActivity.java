74f263fb608db4b1cb204208fb5bb0f73fed1291
Mattheo <mattheoioannou@gmail.com>
2018-02-02 18:04:49 -0800

    Added permission request as well as showing explanations when required.

--- app/src/main/java/solarsitingucsc/smartsolarsiting/MainActivity.java
+++ app/src/main/java/solarsitingucsc/smartsolarsiting/MainActivity.java
@@ -1,8 +1,13 @@
 package solarsitingucsc.smartsolarsiting;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
@@ -26,6 +31,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        checkForPermissions();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[],
+                                           int[] grantResults) {
+        for (int result : grantResults) {
+            if (result != PackageManager.PERMISSION_GRANTED) {
+                showAlert();
+                break;
+            }
+        }
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            openCamera();
+        }
+    }
+
+    private void openCamera() {
         if (checkCameraHardware(this)) {
             mCamera = getCameraInstance();
             mCamera.setDisplayOrientation(90);
@@ -33,6 +56,33 @@ public class MainActivity extends AppCompatActivity {
             FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
             preview.addView(mPreview);
         }
+    }
+
+    private void showAlert() {
+        AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+        alertDialog.setTitle("Alert");
+        alertDialog.setMessage("Your camera and location permissions need to be granted" +
+                " for this app to work. Please enable them through:" +
+                "\n Settings > Apps > Smart Solar Siting > Permissions, and restart the " +
+                "app");
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        alertDialog.show();
+    }
+
+    private void checkForPermissions() {
+        String[] permissions = {Manifest.permission.CAMERA,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION};
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_GRANTED) {
+            openCamera();
+        }
+        ActivityCompat.requestPermissions(this, permissions, 1);
     }
 
     /** Check if this device has a camera */
@@ -48,16 +98,16 @@ public class MainActivity extends AppCompatActivity {
 
     /** A safe way to get an instance of the Camera object. */
     public Camera getCameraInstance(){
-        Camera c = null;
+        Camera camera = null;
         try {
             // attempt to get a Camera instance
-            c = Camera.open();
+            camera = Camera.open();
         }
         catch (Exception e){
             // Camera is not available (in use or does not exist)
         }
 
         // returns null if camera is unavailable
-        return c;
+        return camera;
     }
 }

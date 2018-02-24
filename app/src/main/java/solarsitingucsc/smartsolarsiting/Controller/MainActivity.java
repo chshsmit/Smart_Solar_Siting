3a29e4d0a8218ecd63742f90138f355849c92ce8
Mattheo <mattheoioannou@gmail.com>
2018-02-23 18:26:33 -0800

    Added solar path of all months

--- app/src/main/java/solarsitingucsc/smartsolarsiting/Controller/MainActivity.java
+++ app/src/main/java/solarsitingucsc/smartsolarsiting/Controller/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends Activity {
 //                WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         openCamera();
-
+//
 //        checkForPermissions();
     }
 

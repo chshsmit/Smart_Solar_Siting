9a4d803ee3c667cd2b91ba9dcee54f34c85b0e6d
Christopher Smith <chshsmit@ucsc.edu>
2018-03-09 14:56:08 -0800

    Created unit tests for Grena3 algorithm

---
+++ app/src/test/java/solarsitingucsc/smartsolarsiting/Grena3UnitTest.java
@@ -0,0 +1,34 @@
+package solarsitingucsc.smartsolarsiting;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import solarsitingucsc.smartsolarsiting.Model.AzimuthZenithAngle;
+import solarsitingucsc.smartsolarsiting.Model.Grena3;
+
+/**
+ * Created by chrissmith on 3/9/18.
+ */
+
+public class Grena3UnitTest {
+
+    private static final double DELTA = 1.0;
+    GregorianCalendar newDate = new GregorianCalendar(2018, Calendar.MARCH, 9, 14, 47);
+    AzimuthZenithAngle newAngle = Grena3.calculateSolarPosition(newDate, 36.9, -122.03, 68);
+    
+    //The expected azimuth for March 9th, 2018 at 2:47 is 228 degrees
+    @Test
+    public void azimuthReturnedCorrectly() throws Exception {
+        assertEquals(228.0, newAngle.getAzimuth(), DELTA);
+    }
+
+    //The expected zenith for March 9th, 2018 at 2:47 is 54 degrees
+    @Test
+    public void zenithReturnedCorrectly() throws Exception {
+        assertEquals(54.0, newAngle.getZenithAngle(), DELTA);
+    }
+
+}

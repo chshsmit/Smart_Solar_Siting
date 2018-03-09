761e38d6a1469dba12cf6132329ad0c31e855a18
Christopher Smith <chshsmit@ucsc.edu>
2018-03-09 15:00:12 -0800

    Fixed Grena3 unit tests

--- app/src/test/java/solarsitingucsc/smartsolarsiting/Grena3UnitTest.java
+++ app/src/test/java/solarsitingucsc/smartsolarsiting/Grena3UnitTest.java
@@ -16,16 +16,20 @@ import solarsitingucsc.smartsolarsiting.Model.Grena3;
 public class Grena3UnitTest {
 
     private static final double DELTA = 1.0;
-    GregorianCalendar newDate = new GregorianCalendar(2018, Calendar.MARCH, 9, 14, 47);
-    AzimuthZenithAngle newAngle = Grena3.calculateSolarPosition(newDate, 36.9, -122.03, 68);
-    
-    //The expected azimuth for March 9th, 2018 at 2:47 is 228 degrees
+    GregorianCalendar newDate = new GregorianCalendar(2018, Calendar.MARCH,
+            9, 14, 47);
+
+    AzimuthZenithAngle newAngle = Grena3.calculateSolarPosition(newDate, 36.9,
+            -122.03, 68);
+
+    //The expected azimuth for March 9th, 2018 at 2:47pm is 228 degrees
     @Test
     public void azimuthReturnedCorrectly() throws Exception {
         assertEquals(228.0, newAngle.getAzimuth(), DELTA);
     }
 
-    //The expected zenith for March 9th, 2018 at 2:47 is 54 degrees
+    //The expected zenith for March 9th, 2018 at 2:47pm is 54 degrees
+    // (corresponds to elevation of 36 degrees)
     @Test
     public void zenithReturnedCorrectly() throws Exception {
         assertEquals(54.0, newAngle.getZenithAngle(), DELTA);

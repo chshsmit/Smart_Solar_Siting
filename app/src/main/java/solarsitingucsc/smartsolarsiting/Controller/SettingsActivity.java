033a4ad5b20061d74009438130e1e8622ca95cd0
Christopher Smith <chshsmit@ucsc.edu>
2018-04-12 18:49:14 -0700

    Added toolbar to settings activity

--- app/src/main/java/solarsitingucsc/smartsolarsiting/Controller/SettingsActivity.java
+++ app/src/main/java/solarsitingucsc/smartsolarsiting/Controller/SettingsActivity.java
@@ -2,6 +2,8 @@ package solarsitingucsc.smartsolarsiting.Controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 import solarsitingucsc.smartsolarsiting.R;
 
@@ -11,5 +13,10 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        //Toolbar setup
+        Toolbar topToolBar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(topToolBar);
     }
+
 }

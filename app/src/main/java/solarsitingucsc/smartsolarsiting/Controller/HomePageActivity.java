bde67613427360e315e4e14b2ce17ffc448b0ba3
Christopher Smith <chshsmit@ucsc.edu>
2018-04-13 11:45:02 -0700

    Created list element xml and adapter class

--- app/src/main/java/solarsitingucsc/smartsolarsiting/Controller/HomePageActivity.java
+++ app/src/main/java/solarsitingucsc/smartsolarsiting/Controller/HomePageActivity.java
@@ -1,15 +1,26 @@
 package solarsitingucsc.smartsolarsiting.Controller;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.widget.Toolbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import solarsitingucsc.smartsolarsiting.R;
 
@@ -20,36 +31,22 @@ public class HomePageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
 
-        //Toolbar setup
-        Toolbar topToolBar = findViewById(R.id.my_toolbar);
-        setSupportActionBar(topToolBar);
-
-
-        instantiateBottomNavigation();
+        initializeToolbar();
+        initializeListView();
+        initializeBottomNavigation();
     }
 
-
-    private void instantiateBottomNavigation(){
-        BottomNavigationView bottomNavigation = findViewById(R.id.newSolarSite);
-
-        bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                switch (menuItem.getItemId()) {
-                    case R.id.Solar_Site:
-                        //transition to main activity
-                        changeToMainActivity();
-                        break;
-                }
-                return true;
-            }
-        });
-
-    }
 
     //--------------------------------------------------------------------------------------------
     //Toolbar functions
     //--------------------------------------------------------------------------------------------
+
+    private void initializeToolbar(){
+        //Toolbar setup
+        Toolbar topToolBar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(topToolBar);
+    }
+
 
     //ToolBar function to setup res/menu
     @Override
@@ -75,6 +72,110 @@ public class HomePageActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+
+
+    //--------------------------------------------------------------------------------------------
+    //List view functions
+    //--------------------------------------------------------------------------------------------
+
+    //Adapter and list variables
+    private PreviousSiteAdapter siteAdapter;
+    private ArrayList<ListElement> siteList;
+
+    //Constructor
+    private class ListElement {
+        ListElement() {}
+
+        ListElement(String tl) {
+            textLabel = tl;
+        }
+
+        public String textLabel;
+    }
+
+
+    //Adapts the siteList to previous_site_list_element.xml
+    private class PreviousSiteAdapter extends ArrayAdapter<ListElement> {
+
+        int resource;
+        Context context;
+
+        public PreviousSiteAdapter(Context _context, int _resource, List<ListElement> items) {
+            super(_context, _resource, items);
+            resource = _resource;
+            context = _context;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LinearLayout newView;
+
+            ListElement newSiteElement = getItem(position);
+
+            // Inflate a new view if necessary.
+            if (convertView == null) {
+                newView = new LinearLayout(getContext());
+                LayoutInflater vi = (LayoutInflater)
+                        getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                vi.inflate(resource,  newView, true);
+            } else {
+                newView = (LinearLayout) convertView;
+            }
+
+            // Fills in the view.
+
+
+
+            // Set a listener for the whole list item.
+            newView.setTag(newSiteElement.textLabel);
+            newView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+
+            return newView;
+        }
+    }
+
+    private void initializeListView(){
+        siteList = new ArrayList<ListElement>();                          //This chunk of code sets up the adpater for the list view
+        siteAdapter = new PreviousSiteAdapter(this, R.layout.previous_site_list_element, siteList);
+        ListView myListView =(ListView) findViewById(R.id.previous_site_list_view);
+        //myListView.setEmptyView( findViewById(R.id.empty_view) );
+        myListView.setAdapter(siteAdapter);
+        siteAdapter.notifyDataSetChanged();
+    }
+
+    public void addSiteToListView(){
+        siteList.add(new ListElement());
+        siteAdapter.notifyDataSetChanged();
+
+    }
+
+    //--------------------------------------------------------------------------------------------
+    //Bottom Navigation Functions
+    //--------------------------------------------------------------------------------------------
+
+    private void initializeBottomNavigation(){
+        BottomNavigationView bottomNavigation = findViewById(R.id.newSolarSite);
+
+        bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.Solar_Site:
+                        //transition to main activity
+                        changeToMainActivity();
+                        break;
+                }
+                return true;
+            }
+        });
+
     }
 
     //--------------------------------------------------------------------------------------------

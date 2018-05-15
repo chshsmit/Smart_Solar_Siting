98e64b0fa784f6d5eaff789d3ce379fa4a0acf71
Henry Hargreaves <hwhh@st-andrews.ac.uk>
2018-05-15 13:32:21 -0700

    implimenting share button - also added all the google / facebook stuff - working

--- app/src/main/java/solarsitingucsc/smartsolarsiting/Controller/DisplayCalculationsActivity.java
+++ app/src/main/java/solarsitingucsc/smartsolarsiting/Controller/DisplayCalculationsActivity.java
@@ -144,6 +144,8 @@ public class DisplayCalculationsActivity extends AppCompatActivity {
             //Get the image and make google request
             setBitmap();
         }
+
+
     }
 
     //----------------------------------------------------------------------------------------------
@@ -204,6 +206,27 @@ public class DisplayCalculationsActivity extends AppCompatActivity {
         Toolbar topToolBar = findViewById(R.id.results_toolbar);
         topToolBar.setTitle("Results");
         setSupportActionBar(topToolBar);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // User chose the "Settings" item, show the app settings UI...
+                return true;
+
+            case R.id.action_favorite:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 
 

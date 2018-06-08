82a4924fe28c9bbef8dff4f80420c074378f7f0d
Sam Singh <ssingh55@ucsc.edu>
2018-06-07 18:34:33 -0700

    syncing pushes

--- app/src/main/java/solarsitingucsc/smartsolarsiting/Controller/MainActivity.java
+++ app/src/main/java/solarsitingucsc/smartsolarsiting/Controller/MainActivity.java
@@ -188,44 +188,6 @@ public class MainActivity extends Activity {
         return file;
     }
 
-//    /**
-//     * Create a File for saving an image or video in external storage
-//     */
-//    private static File getExternalOutputMediaFile(int type){
-//        // To be safe, you should check that the SDCard is mounted
-//        // using Environment.getExternalStorageState() before doing this.
-//
-//        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
-//                Environment.DIRECTORY_PICTURES), "Smart Solar Siting");
-//        // This location works best if you want the created images to be shared
-//        // between applications and persist after your app has been uninstalled.
-//
-//        // Create the storage directory if it does not exist
-//        if (! mediaStorageDir.exists()){
-//            if (! mediaStorageDir.mkdirs()){
-//                Log.d("Smart Solar Siting", "failed to create directory");
-//                return null;
-//            }
-//        }
-//
-//        // Create a media file name
-//        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-//        File mediaFile;
-//        if (type == MEDIA_TYPE_IMAGE){
-//            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
-//                    "IMG_"+ timeStamp + ".jpg");
-//        } else if(type == MEDIA_TYPE_VIDEO) {
-//            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
-//                    "VID_"+ timeStamp + ".mp4");
-//        } else {
-//            return null;
-//        }
-//
-//        return mediaFile;
-//    }
-
-
-
 
     /*
     * Function that allows button to switch to panorama activity

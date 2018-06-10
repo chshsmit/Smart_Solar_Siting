24d4173a8c2205217f887ec077cc139c91d65d18
Christopher Smith <chshsmit@ucsc.edu>
2018-06-10 16:09:17 -0700

    Base functionality for tutorial video

--- app/src/main/java/solarsitingucsc/smartsolarsiting/Controller/TutorialVideoActivity.java
+++ app/src/main/java/solarsitingucsc/smartsolarsiting/Controller/TutorialVideoActivity.java
@@ -1,7 +1,10 @@
 package solarsitingucsc.smartsolarsiting.Controller;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 import solarsitingucsc.smartsolarsiting.R;
 
@@ -11,5 +14,19 @@ public class TutorialVideoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutorial_video);
+
+        VideoView mVideoView =(VideoView) findViewById(R.id.tutorial_video_view);
+        MediaController mMediaController = new MediaController(this);
+
+        mMediaController.setAnchorView(mVideoView);
+
+        Uri uri = Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.tutorialVideo);
+
+        mVideoView.setMediaController(mMediaController);
+        mVideoView.setVideoURI(uri);
+        mVideoView.requestFocus();
+
+        mVideoView.start();
+
     }
 }

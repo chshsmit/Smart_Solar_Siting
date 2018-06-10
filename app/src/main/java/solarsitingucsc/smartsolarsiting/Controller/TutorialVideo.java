fe27d25668efb703f6df35f622d3a7c7727a7791
Christopher Smith <chshsmit@ucsc.edu>
2018-06-10 12:57:32 -0700

    Base files for video tutorial

---
+++ app/src/main/java/solarsitingucsc/smartsolarsiting/Controller/TutorialVideo.java
@@ -0,0 +1,15 @@
+package solarsitingucsc.smartsolarsiting.Controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import solarsitingucsc.smartsolarsiting.R;
+
+public class TutorialVideo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutorial_video);
+    }
+}
